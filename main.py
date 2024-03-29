e12130c623f04bfdd2806180f5650d02720aeceb
bolthold <bolthold7507@gmail.com>
2024-03-29 07:34:18 +0300

    4 try fix door sense

--- main.py
+++ main.py
@@ -4,30 +4,20 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
-from gpiozero import Button
+import gpiod
 import enum
 
 class DoorState(enum.Enum):
     open = 1
     close = 0
 
-
-door_sense = Button(27)
-door_state = None
 app = FastAPI()
 camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 templates = Jinja2Templates(directory="page")
 
-door_sense.when_pressed = turn_light_off
-door_sense.when_released = turn_light_on
 
-def turn_light_off():
-    door_state_change()
-    pass
 
-def turn_light_on():
-    door_state_change()
-    pass
+
 
 def gen_frames():
     while True:
@@ -43,10 +33,13 @@ def gen_frames():
 
 
 
-def door_state_change():
-    if door_state == DoorState.open:
+def door_state_changed_event(event):
+    if event.type == gpiod.LineEvent.RISING_EDGE:
         door_state = DoorState.close
-    door_state = DoorState.open
+    elif event.type == gpiod.LineEvent.FALLING_EDGE:
+        door_state = DoorState.open
+        
+    
 
 
 @app.get('/')
@@ -60,6 +53,16 @@ def video_feed():
 
 
 if __name__ == '__main__':
+    with gpiod.Chip(0) as chip:
+        lines = chip.get_lines(27)
+        lines.request(0, type=gpiod.LINE_REQ_EV_BOTH_EDGES)
+        while True:
+            ev_lines = lines.event_wait(sec=1)
+            if ev_lines:
+                for line in ev_lines:
+                    event = line.event_read()
+                    door_state_changed_event(event)
+
     try:
         uvicorn.run(app, host='10.21.0.110', port=8080)
     except KeyboardInterrupt:
